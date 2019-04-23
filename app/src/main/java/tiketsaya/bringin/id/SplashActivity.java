56bb24cfcae629b07fe69a68ddcd22c5ccb88d4a
yogiyulianto <yogi@bringin.id>
2019-04-23 23:09:55 +0700

    get started activity

--- app/src/main/java/tiketsaya/bringin/id/SplashActivity.java
+++ app/src/main/java/tiketsaya/bringin/id/SplashActivity.java
@@ -1,15 +1,29 @@
 package tiketsaya.bringin.id;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class SplashActivity extends AppCompatActivity {
 
-
+ImageView ivLogo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        ivLogo = findViewById(R.id.iv_logo_splash);
+
+        ivLogo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent gotoGetStartedActivity = new Intent(SplashActivity.this, GetStartedActivity.class);
+                startActivity(gotoGetStartedActivity);
+                finish();
+            }
+        });
     }
 }
